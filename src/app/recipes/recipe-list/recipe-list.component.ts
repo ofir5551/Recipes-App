181679f95ff21b226896b234165b00375efb86c3
ofir5551 <ofirbenyamin3@gmail.com>
2020-06-20 23:38:00 +0300

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Burger', 'A tasty hamburger', 'https://c.pxhere.com/photos/2d/ad/beef_bread_breakfast_bun_burger_cheese_close_up_delicious-1550741.jpg!d'),
+    new Recipe('Pizza', 'Great Italian Pizza!', 'https://live.staticflickr.com/3176/2976856006_7661f79a15_b.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
