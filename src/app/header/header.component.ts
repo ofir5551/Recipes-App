af9d86f60f2d9fb3c2e9c30cc8d295d723d7cfe3
ofir5551 <ofirbenyamin3@gmail.com>
2020-07-05 01:02:44 +0300

    Implemented databinding and page navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() changePage = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelectPage(page) {
+    this.changePage.emit(page);
   }
-
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('Burger', 'A tasty hamburger', 'https://c.pxhere.com/photos/2d/ad/beef_bread_breakfast_bun_burger_cheese_close_up_delicious-1550741.jpg!d'),
-    new Recipe('Pizza', 'Great Italian Pizza!', 'https://live.staticflickr.com/3176/2976856006_7661f79a15_b.jpg')
+    new Recipe(
+      'Burger',
+      'A tasty hamburger',
+      'https://c.pxhere.com/photos/2d/ad/beef_bread_breakfast_bun_burger_cheese_close_up_delicious-1550741.jpg!d'
+    ),
+    new Recipe(
+      'Pizza',
+      'Great Italian Pizza!',
+      'https://live.staticflickr.com/3176/2976856006_7661f79a15_b.jpg'
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
